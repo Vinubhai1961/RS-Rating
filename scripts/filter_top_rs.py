1fead9231f20e76e57e0c5755905583370a1bafd
Vinubhai <vnpatel030683@gmail.com>
2025-08-02 10:05:56 -0400

    Update filter_top_rs.py

--- scripts/filter_top_rs.py
+++ scripts/filter_top_rs.py
@@ -113,7 +113,7 @@ def generate_opportunity_report(source_file: str, output_file: str):
         (df_remaining['AvgVol10'] > 300000) &
         (df_remaining['RS Percentile'] - df_remaining['3M_RS Percentile'] > 15) &
         (df_remaining['1M_RS Percentile'] - df_remaining['3M_RS Percentile'] > 10) &
-        (df_remaining['Price'] >= 0.98 * df_remaining['52WKH']) &
+       # (df_remaining['Price'] >= 0.98 * df_remaining['52WKH']) &
         (df_remaining['Price'] > 1 * df_remaining['52WKL']) &
         (df_remaining['MCAP'] > 1000)
     ]
